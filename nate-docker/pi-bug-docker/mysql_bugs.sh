d5b2124f4a1af8785ab246ded10be6bf0e113521
nathanwoerheide <nwoerheide@earlyadopterlabs.com>
2015-06-09 12:36:57 -0500

    bugzilla dockerized

---
+++ nate-docker/pi-bug-docker/mysql_bugs.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+  #DB_USER='root'
+  #DB_PASS='crasher1'
+  #DB='mysql'
+
+  #BZ_USER='bugs'
+  #BZ_PASS='1punjab2'
+
+  echo 'logging into db $DB as $DB_USER'
+  cd /var/www/html/
+  mysql -u "root" --password='crasher1' -e "GRANT ALL PRIVILEGES ON bugs.* TO bugs@localhost IDENTIFIED BY '$db_pass'"
+  mysql -u "root" --password="crasher1" -D "mysql" -e "UPDATE user SET Password=PASSWORD('biteme') WHERE User='bugs';FLUSH PRIVILEGES;"
