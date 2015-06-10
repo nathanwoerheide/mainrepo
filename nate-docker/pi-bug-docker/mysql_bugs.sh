c4a393041fd8c7cff98bb27149b255686f7d5825
nathanwoerheide <nwoerheide@earlyadopterlabs.com>
2015-06-10 11:20:24 -0500

    contain running network still working on

--- nate-docker/pi-bug-docker/mysql_bugs.sh
+++ nate-docker/pi-bug-docker/mysql_bugs.sh
@@ -8,7 +8,7 @@
   #BZ_USER='bugs'
   #BZ_PASS='1punjab2'
 
-  echo 'logging into db $DB as $DB_USER'
+  echo 'logging into db $DB as $DB_USER'nw
   cd /var/www/html/
   mysql -u "root" --password='crasher1' -e "GRANT ALL PRIVILEGES ON bugs.* TO bugs@localhost IDENTIFIED BY '$db_pass'"
   mysql -u "root" --password="crasher1" -D "mysql" -e "UPDATE user SET Password=PASSWORD('biteme') WHERE User='bugs';FLUSH PRIVILEGES;"
---
+++ nate-docker/pi-bug-docker/startup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+service mysql start
+service apache2 start
+service ssh start
