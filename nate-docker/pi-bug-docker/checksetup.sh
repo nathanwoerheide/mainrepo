82b3731aa417c5163afdc256f81bcc09aee65d5d
nathanwoerheide <nwoerheide@earlyadopterlabs.com>
2015-06-24 13:06:35 -0500

    del

--- nate-docker/pi-bug-docker/checksetup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /var/www/html
-/usr/bin/perl install-module.pl PatchReader
-/usr/bin/perl install-module.pl Email::Reply
-/usr/bin/perl install-module.pl Apache2::SizeLimit
-/usr/bin/perl install-module.pl Email::Send
-/usr/bin/perl install-module.pl --all
